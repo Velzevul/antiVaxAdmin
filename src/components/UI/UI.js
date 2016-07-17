6a28cd72dac2c290ccf8eb7175a37438b3c3cc3a
Volodymyr Dziubak <volodymyr.dzyubak@gmail.com>
2016-07-17 06:39:24 -0500

    fixed outdated imports

--- src/components/UI/UI.js
+++ src/components/UI/UI.js
@@ -1,8 +1,6 @@
 import Spinner from './Spinner'
 import Button from './Button'
 import LinkButton from './LinkButton'
-import TextInput from './TextInput'
-import TextArea from './TextArea'
 import Alert from './Alert'
 import Input from './Input'
 import Checkbox from './Checkbox'
@@ -10,6 +8,6 @@ import Editor from './Editor'
 import Select from './Select'
 import {ModalWindow, ModalWindowHeader, ModalWindowBody, ModalWindowFooter} from './ModalWindow'
 
-export {Spinner, Button, LinkButton, TextInput, TextArea, Alert,
+export {Spinner, Button, LinkButton, Alert,
   ModalWindow, ModalWindowHeader, ModalWindowBody, ModalWindowFooter,
   Input, Checkbox, Editor, Select}
